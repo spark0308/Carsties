2b8fb9c2f1e73ad591cca3d6ddaa10447c5411ca
Sankalp Poinwar <SankalpP@centiro.se>
2024-08-21 13:24:52 +0530

    End of section 3

---
+++ src/SearchService/Program.cs
@@ -0,0 +1,41 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using Polly;
+using Polly.Extensions.Http;
+using SearchService.Data;
+using SearchService.Models;
+using SearchService.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
+
+var app = builder.Build();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Lifetime.ApplicationStarted.Register(async () =>
+{
+        try
+        {
+                await DbInitializer.Init(app);
+        }
+        catch (Exception ex)
+        {
+                Console.WriteLine(ex);
+        }
+});
+
+
+app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+        => HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.NotFound)
+                .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
